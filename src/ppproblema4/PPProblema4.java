2cd523fd2a14f7670d01d1b70fb1ac68d3c4373b
karen <karen@KarenZ>
2016-10-15 11:11:34 -0500

    problema 2 del proyecto

--- src/ppproblema4/PPProblema4.java
+++ src/ppproblema4/PPProblema4.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ppproblema4;
-
+import java.util.*;
 /**
  *
  * @author karen
@@ -15,7 +15,19 @@ public class PPProblema4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int c;
+        double f,res;
+        c= solicitarC();
+              
+    }   
+    public static int solicitarC(){
+        Scanner entrada= new Scanner(System.in);
+        int c;
+        System.out.println("Introduce Cº a Convertir");
+            c= entrada.nextInt();
+        double f, res;
+        res= ((c * 9/5) + 32);
+        System.out.println("El resultado es:" + res + "fº");
+        return c;
     }
-    
 }
